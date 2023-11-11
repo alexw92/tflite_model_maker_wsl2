553b3843a157119024b3cd10874ebff737256cf2
Alexander Werthmann <AlexanderWerthmann@gmx.de>
2023-11-11 03:29:07 +0100

    improved notebook

--- normalize_csv.py
+++ normalize_csv.py
@@ -1,32 +1,45 @@
 import csv
+import sys
 
-def normalize_coordinates(line, precision=6):
+PRECISION = 6
+
+def normalize_coordinates(input_path, output_path):
+    with open(input_path, 'r') as infile, open(output_path, 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        for line in reader:
+            normalized_line = normalize_line(','.join(line))
+            writer.writerow(normalized_line.split(','))
+
+def normalize_line(line):
     fields = line.split(',')
-    
+
     # Skip lines with insufficient fields
     if len(fields) < 8:
         return line
-    
+
     # Extract numeric values and normalize
     values = [float(field) if field else None for field in fields[3:9]]
-    normalized_values = [round(value / 100, precision) if value is not None else '' for value in values]
-    
+    normalized_values = [round(value / 100, PRECISION) if value is not None else '' for value in values]
+
     # Replace the original values with normalized values
     fields[3:9] = normalized_values
-    
+
     # Join the fields back into a CSV line
     normalized_line = ','.join(map(str, fields))
-    
+
     return normalized_line
 
-# Example usage:
-input_path = '/mnt/c/Users/Fistus/annotations_mlflow_shuffled.csv'
-output_path = '/mnt/c/Users/Fistus/annotations_mlflow_shuffled_n.csv'
+if __name__ == "__main__":
+    # Check if the correct number of command line arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_path> <output_path>")
+        sys.exit(1)
 
-with open(input_path, 'r') as infile, open(output_path, 'w', newline='') as outfile:
-    reader = csv.reader(infile)
-    writer = csv.writer(outfile)
-    
-    for line in reader:
-        normalized_line = normalize_coordinates(','.join(line))
-        writer.writerow(normalized_line.split(','))
+    # Get input path and output path from command line arguments
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    # Call the function with the provided arguments
+    normalize_coordinates(input_path, output_path)
