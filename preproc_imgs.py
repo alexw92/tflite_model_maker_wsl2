e2e6ca0473fb28a982bc3734e23a6d59b9a91488
Alexander Werthmann <werdy@gmx.net>
2023-11-25 02:32:44 +0100

    removed print spamming

--- preproc_imgs.py
+++ preproc_imgs.py
@@ -19,27 +19,31 @@ def resize_images_in_directory(input_directory, output_directory, target_dimensi
 
         # Full path to the output image
         output_path = os.path.join(output_directory, filename)
+        try:
+            # Open the image file
+            with Image.open(input_path) as img:
+                # Check if resizing is necessary
+                if img.size[0] > target_dimension:
+                    #        print(img.size[0])
+                    #        print(target_dimension)
+                            # Calculate the other dimension to maintain the aspect ratio
+                            width_percent = (target_dimension / float(img.size[0]))
+                            target_height = int((float(img.size[1]) * float(width_percent)))
 
-        # Open the image file
-        with Image.open(input_path) as img:
-             # Check if resizing is necessary
-            if img.size[0] > target_dimension:
-                        print(img.size[0])
-                        print(target_dimension)
-                        # Calculate the other dimension to maintain the aspect ratio
-                        width_percent = (target_dimension / float(img.size[0]))
-                        target_height = int((float(img.size[1]) * float(width_percent)))
+                            # Resize the image while preserving the aspect ratio
+                            resized_img = img.resize((target_dimension, target_height), Image.ANTIALIAS)
 
-                        # Resize the image while preserving the aspect ratio
-                        resized_img = img.resize((target_dimension, target_height), Image.ANTIALIAS)
-
-                        # Save the resized image
-                        resized_img.save(output_path)
-                        resized_count = resized_count + 1
-            else:
-                # If no resizing is needed, simply copy the image to the output directory
-                shutil.copy(input_path, output_path)
-                no_resizing = no_resizing + 1
+                            # Save the resized image
+                            resized_img.save(output_path)
+                            resized_count = resized_count + 1
+                else:
+                    # If no resizing is needed, simply copy the image to the output directory
+                    shutil.copy(input_path, output_path)
+                    no_resizing = no_resizing + 1
+        except (IOError, OSError, Image.UnidentifiedImageError) as e:
+            print(f"Error processing {filename}: {e}")       
+    print(f"{no_resizing} images did not need resizing. Copied to {output_path}")
+    print(f"{resized_count} did not need resizing. Copied to {output_path}")             
 
 if __name__ == "__main__":
     # Check if the correct number of command line arguments is provided
@@ -54,5 +58,3 @@ if __name__ == "__main__":
 
     # Call the function with the provided arguments
     resize_images_in_directory(input_directory, output_directory, target_dimension)
-    print(f"{no_resizing} images did not need resizing. Copied to {output_path}")
-    print(f"{resized_count} did not need resizing. Copied to {output_path}")
